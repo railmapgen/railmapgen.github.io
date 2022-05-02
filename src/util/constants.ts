9e97b23d4b6bb05ce4d0adcad25ef015b2b5d11e
Chito Wong <chi.to.wong@outlook.com>
2022-05-02 11:12:49 +0800

    #6 Add rmg-templates

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -2,5 +2,6 @@ export const componentList = {
     rmg: 'Rail Map Generator',
     'rmg-palette': 'RMG Palette',
     'rmg-components': 'RMG Components',
+    'rmg-templates': 'RMG Templates',
     'seed-project': 'Seed Project',
 };
