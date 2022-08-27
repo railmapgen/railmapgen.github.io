26e16760c5ca3aa2ed935fba19f7317b9c1bef21
Chito Wong <chi.to.wong@outlook.com>
2022-08-27 23:53:53 +0800

    #8 Update dependencies and adopt latest RMG family design

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -1,13 +1,10 @@
 import { Flex, Link, ListItem, UnorderedList } from '@chakra-ui/react';
 import React from 'react';
 import WindowHeader from './window-header';
-import { useAppSelector } from '../redux';
 import { componentList } from '../util/constants';
 import StatusDataTable from './data-table/status-data-table';
 
 export default function AppRoot() {
-    const exampleState = useAppSelector(state => state.app.exampleState);
-
     return (
         <Flex direction="column" height="100%" overflow="hidden">
             <WindowHeader />
