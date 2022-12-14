28e8b09cfd7dddbea2e4757717f1c051b146f751
Chito Wong <chi.to.wong@outlook.com>
2022-12-15 00:54:24 +0800

    #17 Add polyfill for legacy browsers

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import { defineConfig, ProxyOptions, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import legacy from '@vitejs/plugin-legacy';
 
 const rmgProxies = [
     '/rmg',
@@ -30,6 +31,10 @@ export default defineConfig({
     plugins: [
         react(),
         checker({ typescript: true, eslint: { lintCommand: 'eslint ./src' } }),
+        legacy({
+            targets: ['defaults', '>0.2%', 'not dead'],
+            modernPolyfills: true,
+        }),
         splitVendorChunkPlugin(),
     ],
     server: {
