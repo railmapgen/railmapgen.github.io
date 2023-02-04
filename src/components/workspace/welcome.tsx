9413fcc29170291a62d50b0050701c4e8eb1e50b
Chito Wong <chi.to.wong@outlook.com>
2023-02-04 16:38:19 +0800

    #21 Show RMP on welcome page

--- src/components/workspace/welcome.tsx
+++ src/components/workspace/welcome.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import { Heading, Image, Text, VStack } from '@chakra-ui/react';
+import { Heading, HStack, Image, SystemStyleObject, Text, VStack } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
+import rmpLogo from '../../images/rmp-logo512.png';
+
+const styles: SystemStyleObject = {
+    flex: 1,
+    justifyContent: 'center',
+    mb: 10,
+
+    '& > div': {
+        mb: 3,
+    },
+
+    '& img': {
+        w: 120,
+        p: 2,
+        backgroundColor: 'white',
+        borderRadius: 12,
+    },
+};
 
 export default function Welcome() {
     const { t } = useTranslation();
 
     return (
-        <VStack flex={1} justifyContent="center" mb={10}>
-            <Image
-                src={import.meta.env.BASE_URL + 'logo512.png'}
-                w={120}
-                p={2}
-                mb={3}
-                backgroundColor="white"
-                borderRadius={12}
-            />
-            <Heading>{t('Welcome to RMG')}</Heading>
+        <VStack sx={styles}>
+            <HStack spacing={3}>
+                <Image src={import.meta.env.BASE_URL + 'logo512.png'} />
+                <Image src={rmpLogo} backgroundColor="white" />
+            </HStack>
+            <Heading>{t('Welcome to RMG and RMP')}</Heading>
             <Text fontSize="lg" textAlign="center">
                 {t('Select an app to start your own rail map design!')}
             </Text>
