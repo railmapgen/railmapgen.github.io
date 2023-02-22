7e922accca47792f2a0f500c122f72157e0ee446
Chito Wong <chi.to.wong@outlook.com>
2023-02-23 02:18:30 +0800

    #NOISSUE Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 
 import { defineConfig, ProxyOptions, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
-import checker from 'vite-plugin-checker';
 import legacy from '@vitejs/plugin-legacy';
 
 const rmgProxies = [
@@ -30,7 +29,6 @@ export default defineConfig({
     base: '/',
     plugins: [
         react(),
-        checker({ typescript: true, eslint: { lintCommand: 'eslint ./src' } }),
         legacy({
             targets: ['defaults', '>0.2%', 'not dead'],
             modernPolyfills: true,
