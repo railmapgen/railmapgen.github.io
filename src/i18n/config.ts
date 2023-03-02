cc1346453f7ec2d9ea878918f6d0b118d30b6ebd
Chito Wong <chi.to.wong@outlook.com>
2023-03-02 19:01:19 +0800

    #23 Update language model

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,14 +1,15 @@
 import rmgRuntime from '@railmapgen/rmg-runtime';
 import { initReactI18next } from 'react-i18next';
-import RmgTranslate, { LanguageCode, Translation } from '@railmapgen/rmg-translate';
+import { defaultTranslation } from '@railmapgen/rmg-translate';
 import enTranslation from './translations/en.json';
 import zhHansTranslation from './translations/zh-Hans.json';
 import zhHantTranslation from './translations/zh-Hant.json';
 import koTranslation from './translations/ko.json';
 
-const i18n = new RmgTranslate.Builder()
+const i18n = new rmgRuntime.I18nBuilder()
     .use(initReactI18next)
     .withLng(rmgRuntime.getLanguage())
+    .withDefaultResource(defaultTranslation)
     .withResource('en', enTranslation)
     .withResource('zh-Hans', zhHansTranslation)
     .withResource('zh-Hant', zhHantTranslation)
@@ -16,16 +17,3 @@ const i18n = new RmgTranslate.Builder()
     .build();
 
 export default i18n;
-
-export const handleLanguageChange = (language: string) => {
-    i18n.changeLanguage(language).then();
-};
-rmgRuntime.onLanguageChange(handleLanguageChange);
-
-export const translateText = (translation: Translation): string => {
-    return (
-        i18n.languages.map(lang => translation[lang as LanguageCode]).find(name => name !== undefined) ??
-        translation.en ??
-        '(Translation Error)'
-    );
-};
