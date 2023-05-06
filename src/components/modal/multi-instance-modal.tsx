9c26d91785bb5a87ac0c60c2fb18850f925414fe
Chito Wong <chi.to.wong@outlook.com>
2023-05-06 18:22:57 +0800

    #29 Remove close current window button as it is not working

--- src/components/modal/multi-instance-modal.tsx
+++ src/components/modal/multi-instance-modal.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from '@chakra-ui/react';
+import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
 const emptyCallback = () => {
@@ -11,11 +11,13 @@ export default function MultiInstanceModal() {
     return (
         <Modal isOpen={true} onClose={emptyCallback}>
             <ModalContent>
-                <ModalHeader>{t('Rail Map Toolkit is opened in another tab')}</ModalHeader>
-                <ModalBody>{t('You cannot open multiple Rail Map Toolkit at the same time.')}</ModalBody>
+                <ModalHeader>{t('Rail Map Toolkit is opened in another window')}</ModalHeader>
+                <ModalBody>
+                    {t('You cannot open multiple Rail Map Toolkit at the same time. Please close this window.')}
+                </ModalBody>
 
                 <ModalFooter>
-                    <Button>{t('Close current tab')}</Button>
+                    {/*<Button onClick={() => window.close()}>{t('Close current tab')}</Button>*/}
                 </ModalFooter>
             </ModalContent>
         </Modal>
