7ba147fc5470d244539d6e76ea227df25f3be728
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2023-06-10 22:34:14 +0800

    fix

--- s.py
+++ s.py
@@ -1,5 +1,5 @@
 from datetime import date
 import os
-f=os.open(os.getenv('GITHUB_ENV'), 'a')
-f.write(f'date={date.today()}')
+f=open(os.getenv('GITHUB_ENV'), 'a')
+f.write('date='+date.today())
 f.close()
