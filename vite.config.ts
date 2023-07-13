522411679e2702d2675c22cf8b3761244efd95ef
Chito Wong <chi.to.wong@outlook.com>
2023-07-13 21:06:50 +0800

    #NOISSUE Enhance chunks splitting strategy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 
-import { defineConfig, ProxyOptions, splitVendorChunkPlugin } from 'vite';
+import { defineConfig, ProxyOptions } from 'vite';
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
 
@@ -33,8 +33,17 @@ export default defineConfig({
             targets: ['defaults', '>0.2%', 'not dead'],
             modernPolyfills: true,
         }),
-        splitVendorChunkPlugin(),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    react: ['react', 'react-dom', 'react-router-dom', '@reduxjs/toolkit', 'react-i18next'],
+                    chakra: ['@chakra-ui/react', '@emotion/react', '@emotion/styled', 'framer-motion', 'react-icons'],
+                },
+            },
+        },
+    },
     server: {
         proxy: {
             ...rmgProxies,
