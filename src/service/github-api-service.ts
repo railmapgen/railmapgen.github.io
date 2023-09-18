d14408567e88820739c6e93ab982d6361adbf393
Chito Wong <chi.to.wong@outlook.com>
2023-09-18 21:56:44 +0800

    #48 Support GitHub contributors API pagination

--- src/service/github-api-service.ts
+++ src/service/github-api-service.ts
@@ -1,6 +1,22 @@
-export const getContributors = async (repo: string): Promise<string[]> => {
+const getContributorsInner = async (repo: string, page: number): Promise<string[]> => {
     const url = `https://api.github.com/repos/railmapgen/${repo}/contributors`;
-    const res = await fetch(url);
+    const searchParams = new URLSearchParams({ per_page: '100', page: page.toString() });
+    const res = await fetch(url + '?' + searchParams.toString());
     const data = await res.json();
     return data.map((d: Record<string, any>) => d.login);
+};
+
+export const getContributors = async (repo: string): Promise<string[]> => {
+    const result = [];
+    let page = 1;
+    while (true) {
+        const pageResult = await getContributorsInner(repo, page);
+        result.push(...pageResult);
+        if (pageResult.length < 100) {
+            break;
+        } else {
+            page++;
+        }
+    }
+    return result;
 };
