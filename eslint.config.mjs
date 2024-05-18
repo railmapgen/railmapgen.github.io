e0079f154ded24bf40176e77989afd17778f1ecc
Chito Wong <chi.to.wong@outlook.com>
2024-05-18 18:11:59 +0800

    #NOISSUE Update eslint to use flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,30 @@
 // @ts-check
 
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 import prettier from 'eslint-plugin-prettier';
 
-const { plugins: _1, parserOptions: _2, ...reactConfigRecommended } = react.configs.recommended;
-const { plugins: _3, parserOptions: _4, ...reactJsxRuntime } = react.configs['jsx-runtime'];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 export default tseslint.config(
     eslint.configs.recommended,
     {
-        files: ['**/*.{js,jsx,ts,tsx}'],
+        files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
         extends: [...tseslint.configs.recommended],
         rules: {
             '@typescript-eslint/no-explicit-any': 'off',
         },
     },
     {
-        files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-        plugins: { react },
-        extends: [reactConfigRecommended, reactJsxRuntime],
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        extends: [...compat.config(react.configs.recommended), ...compat.config(react.configs['jsx-runtime'])],
         settings: {
             react: {
                 version: 'detect',
