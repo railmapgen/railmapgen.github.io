56da34af6e2c40d0c4113763f60f511ac2d2778d
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2025-02-05 17:46:58 +0800

    #168 Remove happy Chinese New Year

--- src/components/menu/nav-menu.tsx
+++ src/components/menu/nav-menu.tsx
@@ -113,10 +113,6 @@ export default function NavMenu() {
 
                 {menuView === 'main' && <AccountStatus />}
 
-                <Alert status="error">
-                    <AlertDescription>🎇 {t('Happy Chinese New Year!')}</AlertDescription>
-                </Alert>
-
                 {/* menu-body */}
                 <Flex className="nav-menu__body">
                     {menuView === 'main' ? (
